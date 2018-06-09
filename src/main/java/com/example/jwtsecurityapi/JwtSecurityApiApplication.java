3960a8f70dee470fbaef64451ea36aca891d59bb
Kumar Rohela <vkunr@allstate.com>
2018-06-09 23:30:06 +0530

    jwt implement for secuirt

---
+++ src/main/java/com/example/jwtsecurityapi/JwtSecurityApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.jwtsecurityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityApiApplication.class, args);
+	}
+}
